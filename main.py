22a1c3e396729bf0f0f49e39791a54be6b01a514
和久田康平 <kwaku2270@gmail.com>
2022-02-09 19:55:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,57 +4,24 @@ import pandas as pd
 from PIL import Image
 import time
 
-
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
 st.title('研究オリジナルTシャツ　公開！')
 
+pass_true=st.secrets["password"]
+pass_st=0
+pass_in = st.text_input('PASSWORD')
+
+if pass_st==0:
+  if pass_in==pass_true:
+    #st.write("OK")
+    pass_st=1
+  elif pass_in=='':
+    st.write("パスワードを入力し、Enterキーを押してください")
+  else:
+    st.write("パスワードが違います")
 
 
-# st.write('DataFrame')
+  if pass_st==1:
 
-# df=pd.DataFrame({
-#     '１列目':[1,2,3,4],
-#     '２列目':[10,20,30,40]
-# })
+    img=Image.open('utme.JPG')
+    st.image(img,caption='utme',use_column_width=True)
 
-# st.write(df)
-# st.write(df.style.highlight_max(axis=1))
-# st.table(df.style.highlight_max(axis=0))
-
-# """
-
-# # 章
-# ## 節
-# ### 項
-
-# """
-
-# df=pd.DataFrame(
-#     np.random.rand(20,3),
-#     columns=['a','b','c']
-# )
-# st.line_chart(df)#折れ線グラフ
-# st.area_chart(df)#折れ線塗りつぶし
-# st.bar_chart(df)#棒グラフ
-
-# df=pd.DataFrame(
-#     np.random.rand(100,2)/[50,50]+[35.69,139.70],
-#     columns=['lat','lon']
-# )
-# st.map(df)
-
-img=Image.open('utme.JPG')
-st.image(img,caption='utme',use_column_width=True)
-
-#st.write('プログレスバーの表示')
-# 'Start!!'
-
-# latest_iteration=st.empty()
-# bar=st.progress(0)
-
-# for i in range(100):
-#     latest_iteration.text(f'Iteration{i+1}')
-#     bar.progress(i+1)
-#     time.sleep(0.3)
