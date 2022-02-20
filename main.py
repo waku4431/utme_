bcd1766b374d792ad8852b27a849e9134e6631b2
和久田康平 <kwaku2270@gmail.com>
2022-02-20 14:07:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,22 +6,6 @@ import time
 
 st.title('研究オリジナルTシャツ　公開！')
 
-pass_true=st.secrets["password"]
-pass_st=0
-pass_in = st.text_input('PASSWORD')
-
-if pass_st==0:
-  if pass_in==pass_true:
-    #st.write("OK")
-    pass_st=1
-  elif pass_in=='':
-    st.write("パスワードを入力し、Enterキーを押してください")
-  else:
-    st.write("パスワードが違います")
-
-
-  if pass_st==1:
-
-    img=Image.open('utme.JPG')
-    st.image(img,caption='utme',use_column_width=True)
+img=Image.open('utme.JPG')
+st.image(img,caption='utme',use_column_width=True)
 
