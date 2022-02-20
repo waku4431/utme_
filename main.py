757101ef60b4954ddab123b14de3f5a782b830f1
和久田康平 <kwaku2270@gmail.com>
2022-02-20 23:35:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 from PIL import Image
 import time
 
-st.title('研究オリジナルTシャツ！')
+st.title('研究オリジナルTシャツ　公開！')
 
 img=Image.open('utme.JPG')
 st.image(img,caption='utme',use_column_width=True)
